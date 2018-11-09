fe7c39b61b14e4ba1fcdcb0f07d8b12d03e02013
Jennifer Hikichi <jenniferhikichi@Jennifers-MBP.home>
2018-11-09 10:25:18 -1000

    server is running, middleware and route installed

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const Users = require('./models/users');
 // require packages
-
+const express = require('express');
+const bodyParser = require('body-parser');
 // invoke express
-
+const app = express();
 // add middleware
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
 
 // add routes
-// app.get('/', (req, res) => {
-//   res.send('smoke test');
-// });
+app.get('/', (req, res) => {
+  res.send('smoke test');
+});
 
 module.exports = app;
 
